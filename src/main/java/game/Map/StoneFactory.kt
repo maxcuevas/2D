debc1272c60446f3eccdb7099e179e98adb55456
maxcuevas <maximilianocuevas777@gmail.com>
2018-11-30 07:15:20 -0800

    Add stone events to display mousing over.

--- src/main/java/game/Map/StoneFactory.kt
+++ src/main/java/game/Map/StoneFactory.kt
@@ -1,7 +1,9 @@
 package game.Map
 
+import javafx.scene.input.MouseEvent
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
+import javafx.scene.shape.Shape
 import java.awt.geom.Rectangle2D
 
 class StoneFactory {
@@ -10,6 +12,26 @@ class StoneFactory {
     private val height: Double = 5.0
 
     fun create(x: Double, y: Double): Stone {
-        return Stone(ObstructionImpl(false, Rectangle2D.Double(x, y, width, height), Circle(5.0, Color.SLATEGRAY)))
+        val circle = Circle(5.0, Color.SLATEGRAY)
+        circle.stroke = Color.BLACK
+        circle.strokeWidth = 0.0
+        setEvents(circle)
+        return Stone(ObstructionImpl(false, Rectangle2D.Double(x, y, width, height), circle))
     }
+
+
+    private fun setEvents(item: Shape) {
+        item.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                { event -> item.strokeWidth = 2.0 })
+
+        item.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                { event -> item.fill = Color.BLACK })
+
+        item.addEventHandler(MouseEvent.MOUSE_EXITED,
+                { event -> item.strokeWidth = 0.0 })
+
+
+    }
+
+
 }
